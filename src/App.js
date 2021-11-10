93daeb751109986b742100b1d8e0adafb3486979
olga <oluciams84@gmail.com>
2021-11-09 20:18:14 -0500

    Adiciona estado y funcion para mostart la longitud de textarea

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
+function App () {
+
+ const [counter, setCounter] = useState(0);
+ const [text, setText] = useState();
+  
+ const handleCounter= (e)=>{
+  let message = e.target.value
+  let size= message.length  
+  setText(message)
+  setCounter(size)
+
+}  
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea value={text} onChange={handleCounter} rows="3"></textarea>
+        <div className="counter">{counter}</div>
       </div>
     );
-  }
+  
 }
 
 export default App;
+
